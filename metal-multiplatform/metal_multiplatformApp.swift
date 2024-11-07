5cb05bf84c1e7a83741ac9f97d95a56415a6040c
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-11-06 18:28:28 -0600

    Initial Commit

---
+++ metal-multiplatform/metal_multiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  metal_multiplatformApp.swift
+//  metal-multiplatform
+//
+//  Created by Niels Ouvrard on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct metal_multiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ metal-multiplatformTests/metal_multiplatformTests.swift
@@ -0,0 +1,16 @@
+//
+//  metal_multiplatformTests.swift
+//  metal-multiplatformTests
+//
+//  Created by Niels Ouvrard on 06/11/2024.
+//
+
+import Testing
+
+struct metal_multiplatformTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
